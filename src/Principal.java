69a5351f83c7625483ff348e7936eacac8855eb7
Felipe Jorge <felipe.jorge.2g@gmail.com>
2023-11-26 14:26:35 -0300

    adicionando comentarios importantes

--- src/Principal.java
+++ src/Principal.java
@@ -27,7 +27,8 @@ public class Principal {
 			List<TrocoMoeda> troco = troco(moedas, valor);
 			for (int j = 0; j < troco.size(); j++) {
 				TrocoMoeda trocoMoeda = troco.get(j);
-				if(trocoMoeda.moeda >= 200) {
+				if(trocoMoeda.moeda >= 200) {  // imprime as notas.
+					//A lógia é que se o valor em centavos é mmaior ou igual a 200, então já tenho uma nota de 2 reais.
 					System.out.println("Funcionário " + (i+1) + " - Nota: " + trocoMoeda.moeda/100.0 + ", Quantidade: " + trocoMoeda.quantidade);
 				} else {
 					System.out.println("Funcionário " + (i+1) + " - Moeda: " + trocoMoeda.moeda/100.0 + ", Quantidade: " + trocoMoeda.quantidade);
@@ -44,14 +45,14 @@ public class Principal {
 		for (int i = 0; i < moedas.length; i++) {
 			moedasList.add(moedas[i]);
 		}
-		Collections.sort(moedasList);
-		Collections.reverse(moedasList);
+		Collections.sort(moedasList); // ordena a lista de moedae e notas
+		Collections.reverse(moedasList); // reverte a ordem. isto é nessesário para que o algoritmo guloso possa pegar sempre a maior nota primeirro
 		for (int i = 0; i < moedasList.size(); i++) {
 			int moeda = moedasList.get(i);
-			int quantidade = valor / moeda;
-			if (quantidade > 0) {
-				troco.add(new TrocoMoeda(moeda, quantidade));
-				valor -= moeda * quantidade;
+			int quantidade = valor / moeda; // este comando calcula a quantidade de moedas com base no valor
+			if (quantidade > 0) { // 
+				troco.add(new TrocoMoeda(moeda, quantidade));// adciona a moeda ja on=btida a lista de resultados
+				valor -= moeda * quantidade; // calcula o valor do salario que ainda sobra para fazer o troco
 			}
 		}
 		return troco;
